bbb75712c347ba62508c3d78ff080a3fcfb07c29
Miklós Koren <miklos.koren@gmail.com>
2014-10-01 20:57:25 -0400

    IV: add post office count and do 2SLS

--- research_designs/instruments/first_stage.do
+++ research_designs/instruments/first_stage.do
@@ -1,4 +1,6 @@
-local RIVERS arkansas colorado columbia connecticut delaware hudson  missouri ohio red snake tennessee mississippi
+set more off
+set matsize 5000, permanently
+local RIVERS arkansas colorado columbia connecticut delaware hudson  missouri ohio red snake tennessee
 
 tempfile rivers
 clear
@@ -17,9 +19,28 @@ local N = r(max)
 
 forval n=1/`N' {
 	gen river_mile_`n' = river_mile*(river_number==`n')
+	gen river_mile_sq_`n' = river_mile^2*(river_number==`n')
 }
 
 gen segment = int(river_mile*1.6/30)
 egen cluster = group(river segment)
 
-reg bridges river_mile_* water_covered_area, cluster(cluster)
+gen log_post_office_count = log(post_office_count)
+
+reg bridges river_mile_* water_covered_area, cluster(cluster) 
+ivreg log_post_office_count (bridges = water_covered_area) river_mile_*,  cluster(cluster) 
+/*
+Instrumental variables (2SLS) regression               Number of obs =    1206
+                                                       F( 23,   424) =   56.35
+                                                       Prob > F      =  0.0000
+                                                       R-squared     =  0.5867
+                                                       Root MSE      =  .64549
+
+                                  (Std. Err. adjusted for 425 clusters in cluster)
+----------------------------------------------------------------------------------
+                 |               Robust
+log_post_offic~t |      Coef.   Std. Err.      t    P>|t|     [95% Conf. Interval]
+-----------------+----------------------------------------------------------------
+         bridges |   .2954681    .164803     1.79   0.074    -.0284645    .6194006
+
+*/
--- research_designs/instruments/instruments_by_segment.py
+++ research_designs/instruments/instruments_by_segment.py
@@ -4,6 +4,7 @@ from shapely.geometry import shape, Polygon, MultiPoint, MultiPolygon
 from shapely import speedups
 import sys
 from project_geojson import ProjectedFeature
+from quadtree import QuadTree, Feature, point_in_rectangle
 
 def disc_10km(geometry_in_meters):
 	return geometry_in_meters.buffer(10000.0)
@@ -35,6 +36,14 @@ speedups.enable()
 
 river = sys.argv[1]
 
+geojson = json.load(open("../../data/us-postal-history/consistent/postoffices.geojson"))
+points = []
+for point in geojson['features']:
+	projected = ProjectedFeature(point['geometry'], projection='wgs84').lcc
+	points.append((projected.x, projected.y))
+post_offices = QuadTree(points)
+
+
 river_line_meters = ProjectedFeature(json.load(open("../../data/rivers/%s/river.geojson" % river))['features'][0]['geometry'], 'wgs84').lcc
 bridges = json.load(open("../../data/rivers/%s/bridges.geojson" % river))['features']
 bridge_collection = ProjectedFeature(MultiPoint([shape(bridge['geometry']) for bridge in bridges]), projection='wgs84').lcc
@@ -43,12 +52,13 @@ segments = [dict(river_mile=point['river_mile'],
 water_body_fc = json.load(open("../../data/rivers/%s/river_poly.geojson" % river))
 water_body = ProjectedFeature(feature_collection_to_multipolygon(water_body_fc), projection='epsg3975').lcc
 
-writer = csv.DictWriter(sys.stdout, fieldnames=['river_mile', 'water_covered_area', 'bridges'])
+writer = csv.DictWriter(sys.stdout, fieldnames=['river_mile', 'water_covered_area', 'bridges', 'post_office_count'])
 writer.writeheader()
 
 for segment in segments:
 	water_covered_area = segment['geometry'].intersection(water_body.buffer(0)).area
 	bridges = segment['geometry'].intersection(bridge_collection)
+	post_office_count = post_offices.count_overlapping_points(Feature(geometry=segment['geometry']))
 	if bridges.is_empty:
 		number = 0
 	elif bridges.geom_type=='Point':
@@ -57,4 +67,5 @@ for segment in segments:
 		number = len(bridges.geoms)
 	writer.writerow(dict(river_mile=segment['river_mile'], 
 		water_covered_area=water_covered_area/segment['geometry'].area,
-		bridges=number))
+		bridges=number,
+		post_office_count=post_office_count))
