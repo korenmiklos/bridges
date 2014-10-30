528a5eee3a835750a203f21954f8008de8a49128
Miklós Koren <miklos.koren@gmail.com>
2014-10-30 20:59:59 +0100

    API-breaking change: list all post offices within 20km of bridge, with from/to dates and distance

--- research_designs/convergence/count_left_right_post_offices.py
+++ research_designs/convergence/count_left_right_post_offices.py
@@ -5,6 +5,7 @@ from shapely.geometry import shape, Polygon, MultiPoint
 from shapely import speedups
 import sys
 from project_geojson import ProjectedFeature
+from copy import deepcopy
 
 def disc(geometry, radius=5.0):
 	return ProjectedFeature(ProjectedFeature(geometry, 'wgs84').lcc.buffer(radius*1000.0), 'lcc').wgs84
@@ -17,6 +18,12 @@ def get_every_10k_points(line_in_meters):
 		points.append(dict(river_mile=int(covered/1600.0), geometry=line_in_meters.interpolate(covered)))
 		covered += 10000.0
 	return points
+
+def wgs84_distance(point1, point2):
+	'''
+	Distance between two WGS84 points in kilometers.
+	'''
+	return ProjectedFeature(point1, 'wgs84').lcc.distance(ProjectedFeature(point2, 'wgs84').lcc)/1000.0
 
 speedups.enable()
 
@@ -38,13 +45,10 @@ control_regions_10 = [region for region in potential_control_regions if not disc
 left_bank = json.load(open("../../data/rivers/%s/left_bank.geojson" % river))['geometry']
 right_bank = json.load(open("../../data/rivers/%s/right_bank.geojson" % river))['geometry']
 
-keep = ['name', 'start', 'river_mile', 'lat', 'lon']
+keep = ['id', 'name', 'start', 'river_mile', 'lat', 'lon']
 writer = csv.DictWriter(sys.stdout, 
 	fieldnames=keep+
-	['left_5km_%s' % decade for decade in decades]+
-	['right_5km_%s' % decade for decade in decades]+
-	['left_10km_%s' % decade for decade in decades]+
-	['right_10km_%s' % decade for decade in decades]
+	['distance', 'po_from', 'po_to', 'side']
 	)
 writer.writeheader()
 
@@ -64,24 +68,39 @@ for region in control_regions_10:
 	record['geometry'] = region['geometry'].geometry
 	records.append(record)
 
+i = 0
 for record in records:
+	i += 1
+	record['id'] = i
+
 	# save lat lon for future use
 	record['lon'] = record['geometry'].x
 	record['lat'] = record['geometry'].y
 
-	bridge_disc_5 = disc(record['geometry'], radius=5.0) 
-	bridge_disc_10 = disc(record['geometry'], radius=10.0) 
+	bridge = shape(record['geometry'])
 
-	left_side_5 = shape(left_bank).buffer(0).intersection(bridge_disc_5)
-	right_side_5 = shape(right_bank).buffer(0).intersection(bridge_disc_5)
-	left_side_10 = shape(left_bank).buffer(0).intersection(bridge_disc_10)
-	right_side_10 = shape(right_bank).buffer(0).intersection(bridge_disc_10)
+	bridge_disc = disc(record['geometry'], radius=20.0) 
+	left_side = shape(left_bank).buffer(0).intersection(bridge_disc)
+	right_side = shape(right_bank).buffer(0).intersection(bridge_disc)
 
-	for decade in decades:
-		record['left_5km_%s' % decade] = sum([1 for po in post_offices.get_overlapping_points(Feature(geometry=left_side_5)) if po['properties']['from']<=decade])
-		record['right_5km_%s' % decade] = sum([1 for po in post_offices.get_overlapping_points(Feature(geometry=right_side_5)) if po['properties']['from']<=decade])
-		record['left_10km_%s' % decade] = sum([1 for po in post_offices.get_overlapping_points(Feature(geometry=left_side_10)) if po['properties']['from']<=decade])
-		record['right_10km_%s' % decade] = sum([1 for po in post_offices.get_overlapping_points(Feature(geometry=right_side_10)) if po['properties']['from']<=decade])
-	del record['geometry']
-	writer.writerow(record)
+	for po in post_offices.get_overlapping_points(Feature(geometry=left_side)):
+		row = deepcopy(record)
+		# calculate distance to bridge
+		row['distance'] = wgs84_distance(shape(po['geometry']), bridge)
+		row['side'] = 'left'
+		row['po_from'] = po['properties']['from']
+		row['po_to'] = po['properties']['to']
+
+		del row['geometry']
+		writer.writerow(row)
+	for po in post_offices.get_overlapping_points(Feature(geometry=right_side)):
+		row = deepcopy(record)
+		# calculate distance to bridge
+		row['distance'] = wgs84_distance(shape(po['geometry']), bridge)
+		row['side'] = 'right'
+		row['po_from'] = po['properties']['from']
+		row['po_to'] = po['properties']['to']
+
+		del row['geometry']
+		writer.writerow(row)
 
